affeaa94edd9de611d03afdd1b8b3f338c6fd283
rcbus <cleiton.rce@gmail.com>
2020-08-06 21:17:20 -0300

    update_cleiton_20200805_183216

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,19 @@
 import express from 'express';
 import ClassesController from './controllers/ClassesController';
 import ConnectionsController from './controllers/ConnectionsController';
+import UsersController from './controllers/UsersController';
 
 const routes = express.Router();
 const classesController = new ClassesController();
 const connectionsController = new ConnectionsController();
+const userController = new UsersController();
 
 routes.get('/classes', classesController.index);
 routes.post('/classes', classesController.create);
 
 routes.get('/connections', connectionsController.index);
 routes.post('/connections', connectionsController.create);
+
+routes.get('/users', userController.index);
 
 export default routes;
